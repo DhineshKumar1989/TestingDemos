2620622a830e332806163f45f63fd57144242476
DHINESH KUMAR D <45681349+DhineshKumar1989@users.noreply.github.com>
2021-11-11 15:56:40 +0530

    Test Code

---
+++ Calculator Application/Calculator Application/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Application
+{
+  public  class Calculator
+    {
+
+        public int addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Multiplaication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+    }
+}
---
+++ Calculator Application/Calculator ApplicationTests/CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator_Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator_Application.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void additionTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        public void MultiplaicationTest()
+        {
+
+        }
+    }
+}
